db9b4dd1ef13036cc2498179b5f26994581f25ed
guicvsub <rm552321@fiap.com.br>
2024-03-17 19:05:27 -0300

    adicionando login e dicionario

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 
 
 #criando funcoes
-def printal_lista(lista):
-    for elem in lista:
-        print(elem)
-def forca_opcao(lista,msg):
-   printal_lista(lista)
-   opcao = input('selecione uma da opçoes')
-   while opcao not in lista:
-        opcao =input(msg)
-   return opcao
+def set_senha():
+    usuario = input("digite seu usuario")
+    senha = input("digite sua senha")
+    while senha!="adm"and usuario!="adm":
+        print("usuario ou senha invalidos")
+        print("tente novamente")
+        usuario = input("digite seu usuario")
+        senha = input("digite sua senha")
 
 
 
-#criando menu
-acoes=["cadastrar","consultar","deletar","sair"]
-escolha= forca_opcao(acoes," opação invalida! opçao deve conter na lista ")
+log = set_senha()
+
+def printa_chave_valor(dicionario):
+    """Imprime as chaves e valores de um dicionário."""
+    for chave in dicionario:
+        valor = dicionario[chave]
+        print(chave, " ", valor)
+def forca_opcao(lista, msg):
+    printa_chave_valor(lista)
+    opcao = input('Selecione uma das opções: ')
+    while opcao not in [str(chave) for chave in lista]:
+        opcao = input(msg)
+    return opcao
+
+acoes = {1: "cadastrar", 2: "consultar", 3: "deletar", 4: "sair"}
+
+
+
+escolha = forca_opcao(acoes,"erro")
+
 
 
 #criando objeto medicamento
