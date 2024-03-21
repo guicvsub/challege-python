e2cca25a75ec6aa9767c3f92c6b37398ac494c28
guicvsub <rm552321@fiap.com.br>
2024-03-21 08:02:17 -0300

    adcionando base de dados funcao cadastrar

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 
 from tabulate import tabulate
 #base de dados
-medicamentos = [
-    {"id": "id", "nome": "nome", "data_fabricacao": "data_fabricacao", "data_validade": "data_validade"},
-        {"id": 1, "nome": "Paracetamol", "data_fabricacao": "2023-01-15", "data_validade": "2025-01-15"},
-        {"id": 2, "nome": "Amoxicilina", "data_fabricacao": "2023-02-28", "data_validade": "2025-02-28"},
-        {"id": 3, "nome": "Omeprazol", "data_fabricacao": "2023-03-10", "data_validade": "2025-03-10"},
-        {"id": 4, "nome": "Dipirona", "data_fabricacao": "2023-04-05", "data_validade": "2025-04-05"},
-        {"id": 5, "nome": "Ibuprofeno", "data_fabricacao": "2023-05-20", "data_validade": "2025-05-20"}
-    ]
+medicamentos = {
+    "id":["nome","data_fabricacao","data_validade","quantidade","preco_unitario","fabricante"],
 
+    "nome": ["Paracetamol", "Amoxicilina", "Omeprazol", "Dipirona", "Ibuprofeno"],
+    "data_fabricacao": ["2023-01-15", "2023-02-28", "2023-03-10", "2023-04-05", "2023-05-20"],
+    "data_validade": ["2025-01-15", "2025-02-28", "2025-03-10", "2025-04-05", "2025-05-20"],
+    "quantidade": [50, 30, 20, 40, 60],
+    "preco_unitario": [10.50, 20.00, 15.75, 5.90, 8.25],
+    "fabricante": ["Farmax", "Generico", "Medley", "Neo Química", "EMS"]
+}
 
 #criando funcoes
-def set_senha():
+def set_senha():#bug
     usuario = input("digite seu usuario")
     senha = input("digite sua senha")
     while senha!="adm"and usuario!="adm":
@@ -37,11 +38,15 @@ def forca_opcao(lista, msg):
         opcao = input(msg)
     return opcao
 
-acoes = {1: "cadastrar", 2: "consultar", 3: "deletar", 4: "sair"}
+acoes = {"1": "cadastrar", "2": "consultar", "3": "deletar", "4": "sair"}
 
+escolha = forca_opcao(acoes, "erro")
+if escolha== "1":
+    print(f"voce escoljeu {acoes[escolha]}")
+    for key in medicamentos.keys():
+        info = input(f'Diga o/a {key} do novo medicaneto: ')
+        medicamentos[key].append(info)
 
-
-escolha = forca_opcao(acoes,"erro")
 if escolha =="2":
     print(tabulate(medicamentos, headers="firstrow", tablefmt="grid"))
 
@@ -49,4 +54,3 @@ if escolha =="2":
 
 
 
-acoes
